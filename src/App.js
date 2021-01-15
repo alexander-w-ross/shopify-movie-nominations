020b75c2e7542d4424949a870ef344b558c6af20
Alex Ross <alexander.w.rosss@gmail.com>
2021-01-15 13:36:39 -0500

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Container } from "@material-ui/core";
 import "./App.css";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  return <div>test</div>;
+  return (
+    <>
+      <Nav />
+      <Container
+        maxWidth="lg"
+        style={{
+          outline: "5px dotted red",
+          height: "100vh",
+          padding: "10px 10px",
+        }}
+      ></Container>
+    </>
+  );
 }
 
 export default App;
